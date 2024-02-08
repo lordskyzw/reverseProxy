04c658a31426f29546bbccdf09f38d838ea948d6
lordskyzw <lilskyforever@gmail.com>
2024-02-08 21:01:00 +0200

    safe practice i guess

--- server/app.py
+++ server/app.py
@@ -3,21 +3,22 @@ from fastapi.responses import Response
 from botocore.exceptions import ClientError
 from aiobotocore.session import get_session
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
 
 # AWS S3 Configuration
-AWS_ACCESS_KEY_ID = 'AKIATCKAQNC5O5O3O5HE'
-AWS_SECRET_ACCESS_KEY = '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'
+aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 BUCKET_NAME = 'chumbucketzw'
 
 # Initialize S3 client outside the request handler
 session = get_session()
 client = session.create_client('s3', region_name='us-east-1',
-                               aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-                               aws_access_key_id=AWS_ACCESS_KEY_ID)
+                               aws_secret_access_key=aws_secret_access_key,
+                               aws_access_key_id=aws_access_key_id)
 @app.get("/files/{file_name}")
 async def read_file(file_name: str):
     logging.info("Fetching file %s", file_name)
