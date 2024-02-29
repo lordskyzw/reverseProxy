24d9aa85e72bb2432235a5bd5ac28a0d01113e66
lordskyzw <lilskyforever@gmail.com>
2024-02-29 23:17:25 +0200

    shifting from flask run to python

--- reverse proxy/reverseproxy.py
+++ reverse proxy/reverseproxy.py
@@ -114,4 +114,4 @@ def ip_address_to_int(ip_address):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
